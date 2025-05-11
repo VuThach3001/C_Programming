d96923b4ef86d76b07b3d4e244afbc0b0f430bb5
Vu Thach <thach30012@gmail.com>
2025-05-11 11:37:26 +0700

    upload example code and note for video 26

--- project/workspaces_hardcode/project_0/main.c
+++ project/workspaces_hardcode/project_0/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    int  myfavoritenumber = 0;
-    printf("Please enter your favorite number\n");
-    scanf("%d", &myfavoritenumber);
-    printf("Your favorite number: %d\n", myfavoritenumber);
+    // int  myfavoritenumber = 0;
+    // printf("Please enter your favorite number\n");
+    // scanf("%d", &myfavoritenumber);
+    // printf("Your favorite number: %d\n", myfavoritenumber);
+    int total = 0;
+    printf("Total is: %d\n", total);
 
     return 0;
 }
