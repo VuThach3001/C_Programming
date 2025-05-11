31bd1a337dca0dd97f318c18ba7afd4ef6ef74eb
Vu Thach <thach30012@gmail.com>
2025-05-11 11:52:50 +0700

    upload sample code for linker error and note for video 27

--- project/workspaces_hardcode/project_0/main.c
+++ project/workspaces_hardcode/project_0/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+extern int x;
+
 int main(){
     // int  myfavoritenumber = 0;
     // printf("Please enter your favorite number\n");
@@ -7,6 +9,7 @@ int main(){
     // printf("Your favorite number: %d\n", myfavoritenumber);
     int total = 0;
     printf("Total is: %d\n", total);
+    printf("Total is: %d\n", x);
 
     return 0;
 }
