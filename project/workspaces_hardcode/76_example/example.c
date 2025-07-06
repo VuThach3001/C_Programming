0a37560893d54bff1d246a68062e9bf0e9b1603c
Thach <thach30012@gmail.com>
2025-07-06 20:13:30 +0700

    upload note for video 76 and example

---
+++ project/workspaces_hardcode/76_example/example.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void multiplyTwoNumbers(int x, int y);
+
+int main(void)
+{
+    multiplyTwoNumbers(10, 20);
+    multiplyTwoNumbers(20, 30);
+    multiplyTwoNumbers(50, 2);
+
+    return 0;
+}
+
+void multiplyTwoNumbers(int x, int y)
+{
+    int result = x * y;
+    printf("The product of %d multiplied by %d is: %d\n", x, y, result);
+}
