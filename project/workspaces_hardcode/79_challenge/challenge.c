91fd0b0ca3750cf8c36aa0e5c1484b8acceef987
Vu Thach <thach30012@gmail.com>
2025-07-08 09:26:01 +0700

    finish challenge for video 79

--- project/workspaces_hardcode/79_challenge/challenge.c
+++ project/workspaces_hardcode/79_challenge/challenge.c
@@ -5,7 +5,7 @@
 - For this challenge, you are to write three functions in a single program
 
 - Write a function which finds the greatest common divisor of two non-negative integer values and to return the result
-    - GCD, takes two ints as parameters
+    - GCD (greatest common divisor), takes two ints as parameters
 
 - Write a function to calculate the absolute value of a number
     - Should take as a parameter a float and return a float
@@ -16,3 +16,97 @@
     - Should use the absoluteValue function as implemented in the above step
 
 */
+
+#include <stdio.h>
+
+int GCDAlgorithm(int num1, int num2);
+float absoluteValue(float num);
+float squareRootNum(float num);
+
+
+int main(void)
+{
+
+    /* Test algorithm */
+    float absNum = 16;
+    int absIntNum = 12;
+    int numa = -136;
+    int numb = 12;
+    int GCDNum = GCDAlgorithm(numa, numb);
+
+    printf("The GCD of %d and %d is: %d\n", numa, numb, GCDNum);
+
+    float absResult = absoluteValue(absIntNum);
+    printf("Absolute number of %.5f is: %.5f\n", absNum, absResult);
+
+    float squareRootTest = squareRootNum(absNum);
+    printf("Absolute number of %.5f is: %.5f\n", absNum, squareRootTest);
+
+    return 0;
+}
+
+int GCDAlgorithm(int num1, int num2)
+{
+    /* Declare variables */
+    int remainder = 0;
+    int divisor = 0;
+    int dividend = 0;
+    int gcdNum = 0;
+    /* Initial state */
+
+    /* Check if num1 and num2 is negative then rounds them */
+    if (num1 < 0) num1 = -num1;
+    if (num2 < 0) num2 = -num2;
+    
+    /* Check if num1 and num2 are whether zero then return the GCD*/
+    if (num1 == 0) return num2;
+    if (num2 == 0) return num1;
+
+    /* Check whether num1 is greater than num2 and vice versa */
+    if (num1 > num2)
+    {
+        divisor = num2;
+        dividend = num1;
+    }
+    else if(num1 < num2)
+    {
+        divisor = num1;
+        dividend = num2;
+    }
+    else // Check if num1 is equal to num2 then GCD would be itself
+        return num1;
+
+
+    /* GCD Algorithm */
+    while(dividend % divisor != 0)
+    {   
+        remainder = dividend % divisor;
+        dividend = divisor;
+        divisor = remainder;
+    }
+    gcdNum = divisor;
+    return gcdNum;
+}
+
+
+float absoluteValue(float num)
+{
+    if (num < 0.0)
+        num = -num;
+    return num;
+}
+
+float squareRootNum(float num)
+{
+    const float epsilon = .00001;
+    float guess = 1.0;
+    if (num < 0.0)
+    {
+        printf("The heck are you doing?\n");
+        return -1.0;
+    }
+    while (absoluteValue(guess * guess - num) >= epsilon)
+        guess = (num / guess + guess) / 2.0;
+    
+    return guess;
+}
