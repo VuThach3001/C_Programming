05777f5e63bfe7d6b809b33bb9fbc156a9ae5005
Thach <thach30012@gmail.com>
2025-07-18 04:32:15 +0700

    temporarily upload on server to change the laptop

---
+++ project/workspaces_hardcode/89_challenge/challenge.c
@@ -0,0 +1,22 @@
+/*
+- This challenge will help you better understand
+how to use the most common string functions in the string librabry
+
+- Write a program that displays a string in reverse order
+    - Should read input from the keyboard
+    - Need to use the strlen string function
+
+- Write a program that sorts the strings of an array using a bubble sort
+    - Need to use the strcmp and strcpy functions
+
+*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+
+
+    return 0;
+}
